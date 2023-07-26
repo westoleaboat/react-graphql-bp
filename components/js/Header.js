f353d245f2aa39bb1a545e45ebaa7ef17d1a5368
atlasatlas <68698872+westoleaboat@users.noreply.github.com>
2023-07-27 01:18:09 +0300

    initial commit

---
+++ components/js/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+        <section>
+            <a href="/"><img src='https://26159260.fs1.hubspotusercontent-eu1.net/hubfs/26159260/personalBlog/MINDSCAPEClogo.svg' /></a>
+            <nav>
+                <ul>
+                {/* <li><a href="#">About</a></li> */}
+                </ul>
+            </nav>
+        </section>
+    </header>
+  );
+};
+
+export default Header;
